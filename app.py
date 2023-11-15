cb7a20c752fe0d30d3c2198a595e40386109641d
James Gilliland <j.a.e.gilliland@outlook.com>
2023-11-15 14:06:18 +0000

    Py Changes

--- app.py
+++ app.py
@@ -1,42 +1,39 @@
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
+from env import load_dotenv
+
+# Load environment variables from env.py
+load_dotenv()
 
 app = Flask(__name__)
 
+mongo_connection_string = os.getenv("MONGO_CONNECTION_STRING")
+database_name = os.getenv("DATABASE_NAME")
 
+client = MongoClient(mongo_connection_string)
+database = client[database_name]
+recipe_collection = database["Recipe"]
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route('/recipe_form')
-def recipe_form():
-    return render_template('recipe_input.html')
-
-# Create a route to handle form submission
 @app.route('/submit_recipe', methods=['POST'])
 def submit_recipe():
     if request.method == 'POST':
-        name = request.form.get('name')
-        ingredients = request.form.get('ingredients')
-        instructions = request.form.get('instructions')
-        prep_time = request.form.get('prep_time')
-        cook_time = request.form.get('cook_time')
-        servings = request.form.get('servings')
+        recipe_name = request.form['recipe_name']
+        description = request.form['description']
+        instructions = request.form['instructions']
 
-        # Store the recipe in MongoDB
         recipe_data = {
-            'name': name,
-            'ingredients': ingredients,
-            'instructions': instructions,
-            'prep_time': prep_time,
-            'cook_time': cook_time,
-            'servings': servings
+            "RECIPE_NAME": recipe_name,
+            "RECIPE_description": description,
+            "RECIPE_Instructions": instructions
         }
 
-        collection.insert_one(recipe_data)
+        recipe_collection.insert_one(recipe_data)
 
-        
-@app.route('/success')
-def success_page():
-    return render_template('success.html')
+        return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
