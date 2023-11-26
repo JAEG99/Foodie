8d80a8f402bf6d7cd1e83df4ad5ebe619cbc0913
James Gilliland <j.a.e.gilliland@outlook.com>
2023-11-26 19:15:08 +0000

    Folder Changes

--- app.py
+++ app.py
@@ -18,13 +18,57 @@ mongo = PyMongo(app)
 
 
 @app.route("/")
-@app.route("/get_tasks")
-def get_tasks():
-    tasks = mongo.db.tasks.find()
-    return render_template("tasks.html", tasks=tasks)
+def index():
+    return render_template("index.html")
 
+@app.route("/recipe_form")
+def recipe_form():
+    return render_template("recipe_form.html")
+
+@app.route("/submit_recipe", methods=["POST"])
+def submit_recipe():
+    # Get form data
+    recipe_name = request.form.get("name")
+    ingredients = request.form.get("ingredients").split(",")  # Split ingredients into a list
+    instructions = request.form.get("instructions")
+    prep_time = int(request.form.get("prep_time"))
+    cook_time = int(request.form.get("cook_time"))
+    servings = int(request.form.get("servings"))
+
+    # Insert data into MongoDB collections
+    insert_into_ingredients(recipe_name, ingredients)
+    insert_into_recipe(recipe_name, instructions, prep_time, cook_time, servings)
+    insert_into_reviews(recipe_name)
+
+    return redirect(url_for("index"))
+
+def insert_into_ingredients(recipe_name, ingredients):
+    # Placeholder code to insert into the Ingredients collection
+    # Use the db.Ingredients collection
+    ingredients_data = {"recipe_name": recipe_name, "ingredients": ingredients}
+    db.Ingredients.insert_one(ingredients_data)
+
+def insert_into_recipe(recipe_name, instructions, prep_time, cook_time, servings):
+    # Placeholder code to insert into the Recipe collection
+    # Use the db.Recipe collection
+    recipe_data = {
+        "recipe_name": recipe_name,
+        "instructions": instructions,
+        "prep_time": prep_time,
+        "cook_time": cook_time,
+        "servings": servings
+    }
+    db.Recipe.insert_one(recipe_data)
+
+def insert_into_reviews(recipe_name):
+    # Placeholder code to insert into the Reviews collection
+    # Use the db.Reviews collection
+    review_data = {
+        "recipe_name": recipe_name,
+        "review_text": "Placeholder review text",
+        "star_rating": 4
+    }
+    db.Reviews.insert_one(review_data)
 
 if __name__ == "__main__":
-    app.run(host=os.environ.get("IP"),
-            port=int(os.environ.get("PORT")),
-            debug=True)
+    app.run(debug=True)
