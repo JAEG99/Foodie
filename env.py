4b9f8d7d803789a827f819db52bcab1f702ca949
JAEG <j.a.e.gilliland@outlook.com>
2024-01-08 21:01:48 +0000

    Changed env.py

---
+++ env.py
@@ -0,0 +1,6 @@
+import os
+
+os.environ.setdefault("IP", "0.0.0.0")
+os.environ.setdefault("PORT", "5000")
+os.environ.setdefault("MONGO_URI", "mongodb+srv://JAEG9029:pTiBnPgqzxsji0Bv@cluster0.15orj3a.mongodb.net/?retryWrites=true&w=majority")
+os.environ.setdefault("MONGO_DBNAME", "Foodie")
